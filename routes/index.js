ea0a1ae818ab0371c6ea3ef9af88f1bf661e6aa9
Ferenc Gulyas <gulyaasferenc@protonmail.com>
2020-05-15 01:00:16 +0200

    simpler

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,8 @@ const { createPost, registerUser, login, changePassword } = require('./routehand
 const { verifyToken, checkIsAdmin } = require('../middleware/')
 
 router.post('/createPost', verifyToken, checkIsAdmin, createPost)
-router.post('/registerUser', (req, res) => registerUser(req, res))
-router.post('/login', (req, res) => login(req, res))
-router.post('/changePassword', (req, res) => changePassword(req, res))
+router.post('/registerUser', registerUser)
+router.post('/login', login)
+router.post('/changePassword', changePassword)
 
 module.exports = router
